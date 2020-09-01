f41d28f43f71aa7dd107ea8f921ac7af9df5cb5e
Harun Albayrak <52447324+harunalbayrak@users.noreply.github.com>
2020-09-01 14:43:25 +0300

    Added LightWidget & Optimized

--- src/main/java/com/example/demo2/controller/TestController.java
+++ src/main/java/com/example/demo2/controller/TestController.java
@@ -1,24 +1,17 @@
 package com.example.demo2.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
 
 @Controller
 public class TestController {
     @GetMapping("/")
-    public String start(Model model){
-        Deneme deneme = new Deneme(3);
-        List<InstagramPhoto> instagramPhotos = deneme.getInstagramPhotos();
-        model.addAttribute("instagramPhotos",instagramPhotos);
+    public String start(){
         return "index";
     }
 
     @GetMapping("/members")
     public String membersPage() {
-
         return "members";
     }
 }
